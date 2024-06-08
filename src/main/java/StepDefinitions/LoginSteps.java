0d839ae6b8214aa9b7d5ab5e09ec74dc6b90e817
trung <trung.nguyen.spk@gmail.com>
2024-06-08 12:22:39 +0700

    add login feature

--- src/main/java/StepDefinitions/LoginSteps.java
+++ src/main/java/StepDefinitions/LoginSteps.java
@@ -1,28 +1,49 @@
 package StepDefinitions;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginSteps {
-    @Given("user is on login page")
+    WebDriver driver;
+
+    @Given("^browser is open2")
+    public void browserIsOpen2() {
+        System.out.println("Starting browser");
+        String projectPath = System.getProperty("user.dir");
+        System.out.println("projectPath = " + projectPath);
+        System.setProperty("webdriver.chrome.driver", projectPath+ "/src/main/resources/browser/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+        driver.manage().window().maximize();
+        System.out.println("Ending browser");
+    }
+
+    @And("user is on login page")
     public void userIsOnLoginPage() {
-        System.out.println("Inside user is on login page");
+        driver.navigate().to("https://camposcoffee.com/my-account/");
     }
 
     @When("user enters username and password")
     public void userEntersUsernameAndPassword() {
-        System.out.println("Inside user enters username and password");
-    }
-
-    @And("click on login button")
-    public void clickOnLoginButton() {
-        System.out.println("Inside click on login button");
+        driver.findElement(By.id("username")).sendKeys("ganesh20215@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("Codetester@1");
+        driver.findElement(By.name("login")).click();
     }
 
     @Then("user is navigated to the home page")
     public void userIsNavigatedToTheHomePage() {
-        System.out.println("Inside user is navigated to the home page");
+        driver.getPageSource().contains("Hello ganesh20215");
+        driver.close();
+        driver.quit();
     }
 }
--- src/main/resources/Features/Login.feature
+++ src/main/resources/Features/Login.feature
@@ -2,7 +2,7 @@
 # Date: 2024
 Feature: feature to test login function
   Scenario: Check login is successful with valid credentials
-    Given user is on login page
+    Given browser is open2
+    And user is on login page
     When  user enters username and password
-    And click on login button
     Then user is navigated to the home page
